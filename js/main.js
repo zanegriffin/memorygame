1a4f7de6e9ef6997493d53ef707b21680684caf1
Zane <zanebgriffin@gmail.com>
2020-03-11 12:38:37 -0500

    created logic game

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 console.log("Up and running!")
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+
+let cards = [
+"queen", "queen", "king", "king"
+];
+let cardsInPlay = [
+
+] ;
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
 console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!;")
+    } else {
+        alert("sorry try again");
+    }
+}
