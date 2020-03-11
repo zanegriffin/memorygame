b44794672eedb92fedd1e9d8ac6968fddf65aafb
Zane <zanebgriffin@gmail.com>
2020-03-11 14:09:59 -0500

    created logic

--- js/main.js
+++ js/main.js
@@ -6,19 +6,18 @@ let cards = [
 let cardsInPlay = [
 
 ] ;
-
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]){
-        alert("You found a match!;")
-    } else {
-        alert("sorry try again");
+function checkForMatch(){
+    if (cardsInPlay.length === 2) {
+        if (cardsInPlay[0] === cardsInPlay[1]){
+            console.log("You found a match!;")
+        } else {
+            console.log("sorry try again");
+        }
     }
 }
+function flipCard(cardId) {
+  console.log("user flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+flipCard(0);
